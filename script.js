75fd33f1dd0ae9d41d63315092db2ed8a5f1698a
scadler <scadler@s.sfusd.edu>
2020-11-20 03:45:45 +0000

    comp no longer turns into itself, now working on making it avoid tight spaces

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 var ctx = canvas.getContext('2d');
+
 var user = {
     x: 100,
     y: 100,
@@ -18,6 +19,7 @@ var comp = {
                     y: ["0", "-3", "0", "3"],
                 },
     wallFound: false,
+    dead: false,
 }
     ctx.fillStyle = "#000020"
     ctx.rect(0,0,700,700)
@@ -32,9 +34,6 @@ var comp = {
     ctx.lineTo(699, 1);
     ctx.lineTo(1, 1);
     ctx.stroke()
-    ctx.fillStyle = "#FF0001"
-    ctx.rect(300,300,555,500)
-    ctx.fill()
 function draw(color,x,y,d){
     ctx.strokeStyle = color
     ctx.lineWidth = 2;
@@ -57,116 +56,95 @@ function collision(x,y){
     }
 }
 function compAI(){
+    if(comp.dead === false){
     //trying to find way to get average color of all pixels in strip, using that to calc the probablitliyy that the comp will turn
     let w = (comp.direction.x === 0) ? 1 : 100*comp.direction.x/3
     let h = (comp.direction.y === 0) ? 1 : 100*comp.direction.y/3
-  
     pixel = ctx.getImageData(comp.x+comp.direction.x, comp.y+comp.direction.y, w, h).data; 
     pixel = (comp.direction.x > -1 && comp.direction.y > -1) ? pixel.reverse() : pixel;
-    // console.log(index)
-    // console.log(pixel.slice(0,11))
     if(pixel.includes(1) === true){
         comp.wallFound = false
-        //   console.log(pixel.slice(0,11))
         if(pixel.slice(pixel.length-12,pixel.length-1).includes(1) === true){
                 newCompDirection("emergency")
                 console.log("emergency")
-                // console.log(pixel.slice(0,11).includes(1))
             }
         else{ 
             var i = 0
             while(comp.wallFound === false && i < 100){
-    
-            // console.log(pixel)
-            // console.log(pixel.slice(0,11))
-            
             if(pixel.slice(pixel.length-(i*20)-20,pixel.length-(i*20)-1).includes(1) === true){
-                console.log(pixel.slice(pixel.length-(i*20)-20,pixel.length-(i*20)-1).includes(1))
-                console.log(pixel.slice(pixel.length-(i*20)-20,pixel.length-(i*20)-1))
                 comp.wallFound = true
-                console.log(i+" "+0)
-                // console.log(pixel.slice(i*20,(i*20)+19).includes(1)+" "+i)
-                // console.log(Math.pow(i/20, 2))
-                // console.log(Math.random()-((i*i)/400)+" "+i)
+                console.log(Math.pow((20-i)/20, 3)+" "+i)
                 if(Math.random()-(Math.pow((20-i)/20, 3)) < 0){
-                    // console.log("turns")
-                    // console.log("turn"+" "+Math.pow(i/20, 3)+" "+i)
                     newCompDirection("")
                 }
             }
             i++
-            console.log(i+" "+1)
         }
-        //      var i = 0
-        //     while(pixel.slice(i*20,(i*20)+19).includes(1) === false && i < 100){
-        //     console.log("weurchpw9uchrp")
-        //     i++
-        //     // console.log(pixel)
-        //     // console.log(pixel.slice(0,11))
-            
-        //     if(pixel.slice(i*20,(i*20)+19).includes(1) === true){
-        //         // console.log(pixel.slice(i*20,(i*20)+19).includes(1)+" "+i)
-        //         // console.log(Math.pow(i/20, 2))
-        //         if(Math.random()-(Math.pow(i/20, 3)/1) < 0){
-        //             // console.log("turn"+" "+Math.pow(i/20, 3)+" "+i)
-        //             newCompDirection("")
-        //         }
-        //     }
-        // }
-            // console.log(i/100)
         }
     }
-
-    // console.log(hex+" "+2)
+}
+}
+function trapped(){
+    var direcs = ["0","-3","0","3"]
+    var i = 0
+    var n = 0
+    while(i < 4){
+        let w = (parseInt(direcs[i]) === 0) ? 1 : parseInt(direcs[i])/3
+        let h = (parseInt(direcs[(i+1)%4]) === 0) ? 1 : parseInt(direcs[(i+1)%4])/3
+        if(ctx.getImageData(comp.x + parseInt(direcs[i]), comp.y + parseInt(direcs[(i+1)%4]), w, h).data.includes(1)===true){
+            n++
+            if(n===4){
+                comp.direction.x = 0
+                comp.direction.y = 0
+                console.log("failure")
+                comp.dead = true;
+            }
+        }
+        i++
+    }
 }
 function newCompDirection(status){
     var i = 0
-    // if(status === "emergency"){
-    //     comp.dangerDirec.index = Math.floor(Math.random()*4)
-    //     while(i < 1){
-    //         index = (comp.dangerDirec.index + 1) % 4
-    //         posX = parseInt(comp.dangerDirec.x[index])
-    //         posY = parseInt(comp.dangerDirec.y[index])
-    //         console.log(posX+" "+posY)
-    //         let posW = (posX === 0) ? 1 : posX/3
-    //         let posH = (posY === 0) ? 1 : posY/3
-    //         console.log(ctx.getImageData(comp.x+posX, comp.y+posY, posW, posH).data)
-    //         if(ctx.getImageData(comp.x + posX, comp.y + posY, posW, posH).data.includes(1)===false){
-    //             i++
-    //             comp.direction.x = posX
-    //             comp.direction.y = posY
-    //         }
-    //     }
-    // }
-    // else{
+    if(status === "emergency"){
+        trapped()
+        comp.dangerDirec.index = Math.floor(Math.random()*4)
+        while(i < 5){
+            comp.dangerDirec.index = (comp.dangerDirec.index + 1) % 4
+            index = comp.dangerDirec.index
+            posX = parseInt(comp.dangerDirec.x[index])
+            posY = parseInt(comp.dangerDirec.y[index])
+            let posW = (posX === 0) ? 1 : posX/3
+            let posH = (posY === 0) ? 1 : posY/3
+            if(ctx.getImageData(comp.x + posX, comp.y + posY, posW, posH).data.includes(1)===false){
+                comp.direction.x = posX
+                comp.direction.y = posY
+            }
+            i++
+            
+        }
+    }
+    else{
         while(i < 3){
             i++
             posX = (Math.random()-0.5 > 0) ? 0 : (Math.random()-0.5 < 0) ? -3 : 3
             posY = (posX !== 0) ? 0 : (Math.random()-0.5 < 0) ? -3 : 3
-            let posW = (posX === 0) ? 1 : posX/3
-            let posH = (posY === 0) ? 1 : posY/3
+            //I think that the increase multiplier for posX and Y will make the comp avoid
+            //turning into tight spaces, not sure
+            let posW = (posX === 0) ? 1 : posX*4
+            let posH = (posY === 0) ? 1 : posY*4
             if(ctx.getImageData(comp.x+posX, comp.y+posY, posW, posH).data.includes(1)===false){
-
                 if(comp.direction.x !== posX || comp.direction.y !==posY){
                     comp.direction.x = posX
                     comp.direction.y = posY
-                    // console.log("success")
                     i = 3
                 }
-                // else{
-                //     if(i > 10){
-                //         // console.log("failed")
-                //         comp.direction.x = 0
-                //         comp.direction.y = 0
-                //     }
                 }
             }
-        // }
-    // }
+    }
 }
 function move(){
-    user.x += user.direction.x
-    user.y += user.direction.y
+    // user.x += user.direction.x
+    // user.y += user.direction.y
     comp.x += comp.direction.x
     comp.y += comp.direction.y
 }
@@ -178,7 +156,7 @@ function game(){
     compAI()
 }
 game()
-setInterval(game, 50)
+setInterval(game,20)
 
 document.addEventListener('keydown', keyPressed)
 function keyPressed(e) {
